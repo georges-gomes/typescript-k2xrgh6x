bbc956dea8e0e27233bf1ed57fbeb9c1bac54ba8
Georges Gomes <georges.gomes@gmail.com>
2019-11-13 20:53:57 +0000

    First commit

---
+++ src/index.stories.js
@@ -0,0 +1,9 @@
+import { html } from "lit-html";
+
+export const story1 = () => html`
+  <custom-element name="World"></custom-element>
+`;
+
+export const story2 = () => html`
+  <custom-element name="Buddy"></custom-element>
+`;
