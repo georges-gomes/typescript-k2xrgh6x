9891c407c9d4ab3f4a5192999b9d75c35a9b35af
Georges Gomes <georges.gomes@gmail.com>
2019-11-15 15:12:16 +0000

    tests

---
+++ src/index.spec.js
@@ -0,0 +1,14 @@
+// BDD-Style Testing (powered by https://mochajs.org/)
+//
+// Use any renderer for you stories
+// import { fixture } from "@open-wc/testing-helpers";
+//
+// Use any assert library
+import chai from "chai/chai.js";
+const expect = chai.expect;
+
+describe("Dummy test", function() {
+  it("should be always true", function() {
+    expect(true).to.be.true;
+  });
+});
